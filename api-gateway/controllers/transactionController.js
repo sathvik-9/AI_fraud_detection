7f4f437b757b21a4c45ba09465d6c51f9af6177f
Sathvik <sathvik4050@gmail.com>
2026-03-02 10:03:57 +0530

    new API Endpoints added

--- api-gateway/controllers/transactionController.js
+++ api-gateway/controllers/transactionController.js
@@ -28,3 +28,31 @@ export const processTransaction = async (req, res) => {
         res.status(500).json({error: "Error processing transaction"});
     }
 }
+
+export const getAllTransactions = async (req, res) =>{
+    try{
+        const transactions = await Transaction.find().sort({createdAt: -1});
+        res.json(transactions);
+    }
+    catch(error){
+        console.log("Error fetching transactions:", error);
+        res.status(500).json({error: "Error fetching transactions"});
+    }
+}
+
+export const getFraudStats = async (req, res) =>{
+    try{
+        const total = await Transaction.countDocuments();
+        const fraud_count = await Transaction.countDocuments({is_fraud:true});
+        const high_risk = await Transaction.countDocuments({risk_level: "HIGH"});
+        res.json({
+            total_transacitons: total,
+            fraud_transactions: fraud_count,
+            fraud_percentage: total > 0 ? ((fraud_count/total)*100).toFixed(2) : 0,
+            high_risk: high_risk,
+        })
+    }
+    catch(error){
+        console.error("Error fetching fraud status:", error);
+    }
+}
--- api-gateway/routes/transactionRoutes.js
+++ api-gateway/routes/transactionRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import {processTransaction} from "../controllers/transactionController.js";
+import {getAllTransactions} from "../controllers/transactionController.js";
+import {getFraudStats} from "../controllers/transactionController.js";
 
 const router = express.Router();
 
 router.post("/", processTransaction);
+router.get("/", getAllTransactions);
+router.get("/stats", getFraudStats);
 
 
 export default router;
